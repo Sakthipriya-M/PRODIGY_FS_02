bef9f98fdc4ce8ccd2fb85f644e2a5323cb93ef3
Sakthipriya-M <145949672+Sakthipriya-M@users.noreply.github.com>
2024-08-01 18:12:13 +0530

    Create employee.model.ts

---
+++ Employee management system/frontend/employee.model.ts
@@ -0,0 +1,12 @@
+export interface Employee {
+  id?: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone?: string;
+  position?: string;
+  department?: string;
+  dateOfBirth?: string;
+  dateOfHire?: string;
+  salary?: number;
+}
