0deba458175eb712aa3147acbed2cc615f461718
Sakthipriya-M <145949672+Sakthipriya-M@users.noreply.github.com>
2024-08-01 17:53:25 +0530

    Create routing.ts

---
+++ Employee management system/frontend/routing.ts
@@ -0,0 +1,17 @@
+// src/app/app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { EmployeeFormComponent } from './components/employee-form/employee-form.component';
+
+const routes: Routes = [
+  { path: 'employees', component: EmployeeListComponent },
+  { path: 'employees/new', component: EmployeeFormComponent },
+  { path: '', redirectTo: '/employees', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
